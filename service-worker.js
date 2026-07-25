22cbd2990b10642f7db22fb86eb36c22ddae0c7d
Mark Graham <doonleith@icloud.com>
2026-07-25 21:31:40 +0100

    Improve Google sign-in feedback

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'tiramisu-shell-v2';
+const CACHE = 'tiramisu-shell-v3';
 const SHELL = ['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./assets/icon-192.svg','./assets/icon-512.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL))));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
