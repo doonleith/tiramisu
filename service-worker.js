c9d3ae4e1cd6b53a850dee028945eb96c4405789
Mark Graham <doonleith@icloud.com>
2026-07-25 21:49:04 +0100

    Refine landing page wording

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'tiramisu-shell-v8';
+const CACHE = 'tiramisu-shell-v9';
 const SHELL = ['./','./index.html','./styles.css','./app.js?v=6','./manifest.webmanifest','./assets/icon-192.svg','./assets/icon-512.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
